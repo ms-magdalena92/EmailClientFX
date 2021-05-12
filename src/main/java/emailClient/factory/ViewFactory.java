1bf0603aebaf954206d37da17e9634fefa1fce46
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-05-12 10:41:12 +0200

    Fix resizable property setting

--- src/main/java/emailClient/factory/ViewFactory.java
+++ src/main/java/emailClient/factory/ViewFactory.java
@@ -32,7 +32,7 @@ public class ViewFactory {
         Scene scene = new Scene(parent);
         Stage stage = new Stage();
         stage.setScene(scene);
-        stage.setResizable(false);
+        stage.setResizable(resizable);
         stage.show();
     }
 }
