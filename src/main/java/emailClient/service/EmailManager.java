ea33fce2d1003d662b1a29e1046dc173d75a43eb
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-05-13 13:47:15 +0200

    Add folder fetching method and add folders to emails tree view

--- src/main/java/emailClient/service/EmailManager.java
+++ src/main/java/emailClient/service/EmailManager.java
@@ -1,6 +1,7 @@
 package emailClient.service;
 
 import emailClient.model.EmailAccount;
+import emailClient.model.EmailFolder;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -8,11 +9,21 @@ public class EmailManager {
 
     private final ObservableList<EmailAccount> emailAccounts = FXCollections.observableArrayList();
 
+    private final EmailFolder foldersRoot = new EmailFolder("");
+
     public void addEmailAccount(EmailAccount emailAccount) {
         emailAccounts.add(emailAccount);
+        EmailFolder emailFolder = new EmailFolder(emailAccount.getEmailAddress());
+        FetchFolderService fetchFolderService = new FetchFolderService(emailAccount.getStore(), emailFolder);
+        fetchFolderService.start();
+        foldersRoot.getChildren().add(emailFolder);
     }
 
     public ObservableList<EmailAccount> getEmailAccounts() {
         return emailAccounts;
+    }
+
+    public EmailFolder getFoldersRoot() {
+        return foldersRoot;
     }
 }
