3613bf59a04db90de462c50f121e486fcf5bbec7
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-05-13 13:47:52 +0200

    Set up emails tree view

--- src/main/java/emailClient/controller/MainWindowController.java
+++ src/main/java/emailClient/controller/MainWindowController.java
@@ -3,17 +3,21 @@ package emailClient.controller;
 import emailClient.factory.ViewFactory;
 import emailClient.service.EmailManager;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeView;
 import javafx.scene.web.WebView;
 
-public class MainWindowController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController extends BaseController implements Initializable {
 
     private static final String MAIN_VIEW_FILE_NAME = "mainWindow.fxml";
 
     @FXML
-    private TreeView<?> emailsTreeView;
+    private TreeView<String> emailsTreeView;
 
     @FXML
     private TableView<?> emailsTableView;
@@ -38,5 +42,15 @@ public class MainWindowController extends BaseController {
 
     public MainWindowController(ViewFactory viewFactory, EmailManager emailManager) {
         super(viewFactory, emailManager, MAIN_VIEW_FILE_NAME);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setUpEmailsTreeView();
+    }
+
+    private void setUpEmailsTreeView() {
+        emailsTreeView.setRoot(emailManager.getFoldersRoot());
+        emailsTreeView.setShowRoot(false);
     }
 }
