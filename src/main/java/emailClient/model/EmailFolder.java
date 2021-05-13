9a38906e7d66174fe7085aae22c3a0604732d41f
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-05-13 13:45:02 +0200

    Create emails folder model

---
+++ src/main/java/emailClient/model/EmailFolder.java
@@ -0,0 +1,10 @@
+package emailClient.model;
+
+import javafx.scene.control.TreeItem;
+
+public class EmailFolder extends TreeItem<String> {
+
+    public EmailFolder(String name) {
+        super(name);
+    }
+}
