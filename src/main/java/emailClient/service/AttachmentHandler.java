22e5fdc2eea956a91b44fdf51a6038be761f4f42
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-05-27 14:33:23 +0200

    Save attachment to disc

---
+++ src/main/java/emailClient/service/AttachmentHandler.java
@@ -0,0 +1,22 @@
+package emailClient.service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
+import java.io.IOException;
+
+public class AttachmentHandler {
+
+    MimeBodyPart mimeBodyPart;
+
+    private final String downloadedFilePath;
+
+    public AttachmentHandler(MimeBodyPart mimeBodyPart) throws MessagingException {
+        this.mimeBodyPart = mimeBodyPart;
+        String LOCATION_OF_DOWNLOADS = System.getProperty("user.home") + "/Downloads/";
+        this.downloadedFilePath = LOCATION_OF_DOWNLOADS + mimeBodyPart.getFileName();
+    }
+
+    public void saveAttachmentToDisc() throws MessagingException, IOException {
+        mimeBodyPart.saveFile(downloadedFilePath);
+    }
+}
